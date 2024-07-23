335327ab2245fe3b23755b9369ff5b67185106a4
d4ng4nh <dangtuananh_t66@hus.edu.vn>
2024-07-24 03:13:15 +0700

    update models

--- src/models/Showtime.ts
+++ src/models/Showtime.ts
@@ -1,27 +1,38 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '../config/connectDB';
+import Movie from './Movie';
+import Room from './Room';
+
 class Showtime extends Model {
-  public psmId!: number;
-  public roomId!: number;
-  public movieId!: number;
-  public startTime!: TimeRanges;
-  public endTime!: TimeRanges;
-  public space!: string;
+  public id!: string;
+  public movieId!: string;
+  public roomId!: string;
+  public startTime!: Date;
+  public endTime!: Date;
+  public showDate!: Date;
+  public status!: string;
 }
 
 Showtime.init({
-  psmId: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
+  id: {
+    type: DataTypes.STRING,
     primaryKey: true,
   },
-  roomId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
   movieId: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.STRING,
     allowNull: false,
+    references: {
+      model: Movie,
+      key: 'id',
+    },
+  },
+  roomId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: Room,
+      key: 'id',
+    },
   },
   startTime: {
     type: DataTypes.TIME,
@@ -31,15 +42,24 @@ Showtime.init({
     type: DataTypes.TIME,
     allowNull: false,
   },
-  space: {
-    type: DataTypes.STRING,
+  showDate: {
+    type: DataTypes.DATE,
     allowNull: false,
   },
-  
+  status: {
+    type: DataTypes.ENUM('scheduled', 'ongoing', 'completed', 'cancelled'),
+    allowNull: false,
+  },
 }, {
   sequelize,
   modelName: 'Showtime',
+  tableName: 'Showtimes',
+  timestamps: true,
 });
 
+Showtime.belongsTo(Movie, { foreignKey: 'movieId' });
+Showtime.belongsTo(Room, { foreignKey: 'roomId' });
+Movie.hasMany(Showtime, { foreignKey: 'movieId' });
+Room.hasMany(Showtime, { foreignKey: 'roomId' });
 
 export default Showtime;
