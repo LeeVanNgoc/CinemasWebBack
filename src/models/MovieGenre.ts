5526140eca9db0ff93fc8f681821a729507c2402
d4ng4nh <dangtuananh_t66@hus.edu.vn>
2024-07-26 10:17:36 +0700

    fix relational database

--- src/models/MovieGenre.ts
+++ src/models/MovieGenre.ts
@@ -1,29 +1,34 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '../config/connectDB';
-import Genres from './Genres'
+import Genres from './Genres';
+import Movie from './Movie';
 
 class MovieGenre extends Model {
   public movieGenreId!: number;
-  public genreId!: string;
+  public genreId!: number;
 }
 
 MovieGenre.init({
   movieGenreId: {
     type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+    primaryKey: false,
+    references: {
+      model: Movie,
+      key: 'movieid',
+    },
   },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
+  genreId: {
+    type: DataTypes.INTEGER,
+    primaryKey: false,
+    references: {
+      model: Genres,
+      key: 'genreId',
+    },
   },
 }, {
   sequelize,
   modelName: 'MovieGenre',
-  timestamps: false
+  timestamps: false,
 });
-
-MovieGenre.belongsTo(Genres, { foreignKey:'movieGenreId' });
-Genres.hasOne(MovieGenre, { foreignKey:'movieGenreId' });
 
 export default MovieGenre;
