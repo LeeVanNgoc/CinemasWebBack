b950641c15c2378a146400373e68bf1b8c0daa0b
Lee Ngoc <lee.ngoc6462@gmail.com>
2024-08-07 14:27:34 +0700

    Update BookedSeat, News, and API.doc

--- src/controllers/newsController.ts
+++ src/controllers/newsController.ts
@@ -1,49 +1,78 @@
-import { Request, Response } from 'express';
-import { createNews, deleteNews, editNews, getAllNews, getNewsById } from '../services/newsService';
+import { Request, Response } from "express";
+import {
+  createNews,
+  deleteNews,
+  editNews,
+  getAllNews,
+  getNewsById,
+} from "../services/newsService";
 
 const handleCreateNews = async (req: Request, res: Response) => {
-  const data = req.body;
+  const data = req.query;
   try {
     const result = await createNews(data);
     if (result.errCode !== 0) {
-      return res.status(400).json({ errCode: result.errCode, error: result.message });
+      return res
+        .status(400)
+        .json({ errCode: result.errCode, error: result.message });
     }
-    res.status(201).json({ errCode: result.errCode, message: result.message, news: result.news });
+    res.status(201).json({
+      errCode: result.errCode,
+      message: result.message,
+      news: result.news,
+    });
   } catch (error) {
-    res.status(500).json({ errCode: 3, error: `Something went wrong in creating news: ${error}` });
+    res.status(500).json({
+      errCode: 3,
+      error: `Something went wrong in creating news: ${error}`,
+    });
   }
 };
 
 const handleDeleteNews = async (req: Request, res: Response) => {
-  const newsId = parseInt(req.query.newsId as string);
-  if (isNaN(newsId)) {
-    return res.status(400).json({ errCode: 2, error: 'Invalid news ID' });
+  const postId = parseInt(req.query.postId as string);
+  if (isNaN(postId)) {
+    return res.status(400).json({ errCode: 2, error: "Invalid news ID" });
   }
   try {
-    const result = await deleteNews(newsId);
+    const result = await deleteNews(postId);
     if (result.errCode !== 0) {
-      return res.status(404).json({ errCode: result.errCode, error: result.message });
+      return res
+        .status(404)
+        .json({ errCode: result.errCode, error: result.message });
     }
     res.status(200).json({ errCode: result.errCode, message: result.message });
   } catch (error) {
-    res.status(500).json({ errCode: 3, error: `Something went wrong in deleting news: ${error}` });
+    res.status(500).json({
+      errCode: 3,
+      error: `Something went wrong in deleting news: ${error}`,
+    });
   }
 };
 
 const handleEditNews = async (req: Request, res: Response) => {
-  const newsId = parseInt(req.query.newsId as string);
-  if (isNaN(newsId)) {
-    return res.status(400).json({ errCode: 2, error: 'Invalid news ID' });
+  const data = req.query;
+  const postId = Number(data.postId);
+  if (isNaN(postId)) {
+    return res.status(400).json({ errCode: 2, error: "Invalid news ID" });
   }
-  const data = { ...req.body, id: newsId };
   try {
     const result = await editNews(data);
     if (result.errCode !== 0) {
-      return res.status(404).json({ errCode: result.errCode, error: result.message });
+      return res
+        .status(404)
+        .json({ errCode: result.errCode, error: result.message });
     }
-    res.status(200).json({ errCode: result.errCode, message: result.message, news: result.news });
+    res.status(200).json({
+      errCode: result.errCode,
+      message: result.message,
+      news: result.news,
+    });
   } catch (error) {
-    res.status(500).json({ errCode: 3, error: `Something went wrong in editing news: ${error}` });
+    res.status(500).json({
+      errCode: 3,
+      error: `Something went wrong in editing news: ${error}`,
+    });
   }
 };
 
@@ -51,27 +80,45 @@ const handleGetAllNews = async (req: Request, res: Response) => {
   try {
     const result = await getAllNews();
     if (result.errCode !== 0) {
-      return res.status(400).json({ errCode: result.errCode, error: result.message });
+      return res
+        .status(400)
+        .json({ errCode: result.errCode, error: result.message });
     }
-    res.status(200).json({ errCode: result.errCode, message: result.message, news: result.news });
+    res.status(200).json({
+      errCode: result.errCode,
+      message: result.message,
+      news: result.news,
+    });
   } catch (error) {
-    res.status(500).json({ errCode: 3, error: `Something went wrong in getting news: ${error}` });
+    res.status(500).json({
+      errCode: 3,
+      error: `Something went wrong in getting news: ${error}`,
+    });
   }
 };
 
 const handleGetNewsById = async (req: Request, res: Response) => {
-  const newsId = parseInt(req.query.newsId as string);
-  if (isNaN(newsId)) {
-    return res.status(400).json({ errCode: 2, error: 'Invalid news ID' });
+  const postId = Number(req.query.postId);
+  if (isNaN(postId)) {
+    return res.status(400).json({ errCode: 2, error: "Invalid news ID" });
   }
   try {
-    const result = await getNewsById(newsId);
+    const result = await getNewsById(postId);
     if (result.errCode !== 0) {
-      return res.status(404).json({ errCode: result.errCode, error: result.message });
+      return res
+        .status(404)
+        .json({ errCode: result.errCode, error: result.message });
     }
-    res.status(200).json({ errCode: result.errCode, message: result.message, news: result.news });
+    res.status(200).json({
+      errCode: result.errCode,
+      message: result.message,
+      news: result.news,
+    });
   } catch (error) {
-    res.status(500).json({ errCode: 3, error: `Something went wrong in getting news: ${error}` });
+    res.status(500).json({
+      errCode: 3,
+      error: `Something went wrong in getting news: ${error}`,
+    });
   }
 };
 
@@ -80,5 +127,5 @@ export default {
   handleDeleteNews,
   handleEditNews,
   handleGetAllNews,
-  handleGetNewsById
+  handleGetNewsById,
 };
